ddfc0a7830372518f78a718117b628bb97be51cf
Olivia Cheng <olivia@heyitsolivia.com>
2019-02-15 09:49:42 -0500

    Updates

---
+++ pages-manifest-a132322778786deaa0a8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{143:function(e){e.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"},{componentChunkName:"component---src-pages-resume-js",jsonName:"resume-e4f",path:"/resume/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","resume-e4f":"794/path---resume-e-4-f-cdf-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=pages-manifest-a132322778786deaa0a8.js.map
