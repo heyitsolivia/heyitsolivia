f8f365da25a74ce4ea8e7af16250a242ebb53230
Olivia Cheng <olivia@heyitsolivia.com>
2019-02-15 09:49:05 -0500

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,20 @@ module.exports = {
     plugins: [
         'gatsby-plugin-sass',
         'gatsby-plugin-react-helmet',
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-37079640-1",
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**", "/do-not-track/me/too/"],
+            },
+        },
     ]
 }
   
